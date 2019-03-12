3e357837d905f97a5372026a67af4764a3faf1e6
xuhan <xuhan@133.cn>
2019-03-12 11:14:17 +0800

    init

---
+++ src/main/java/com/example/secondhand/SecondhandApplication.java
@@ -0,0 +1,13 @@
+package com.example.secondhand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondhandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondhandApplication.class, args);
+    }
+
+}
